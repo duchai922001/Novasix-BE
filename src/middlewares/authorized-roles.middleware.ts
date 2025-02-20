a92ce8c94148878566c6b621e3cfbfb5ce8a2d82
Duchai232 <Duchai757@gmail.com>
2025-02-20 18:58:29 +0700

    middleware verify

--- src/middlewares/authorized-roles.middleware.ts
+++ src/middlewares/authorized-roles.middleware.ts
@@ -5,7 +5,7 @@ export const authorizeRoles =
   (...allowedRoles: string[]) =>
   (req: Request, res: Response, next: NextFunction): void => {
     try {
-      const userRole = req.user?.role;
+      const userRole = res.locals.user?.role;
       if (!userRole || !allowedRoles.includes(userRole)) {
         res.status(HttpStatus.FORBIDDEN).json({
           message: `Truy cập bị từ chối. Bạn cần một trong những vai trò sau: ${allowedRoles.join(
