00c3abe92a68827340bf62a8ec9f38d151c8a3ad
Duchai232 <Duchai757@gmail.com>
2025-03-21 17:35:05 +0700

    fix end point FE

--- src/presentations/controllers/payment.controller.ts
+++ src/presentations/controllers/payment.controller.ts
@@ -19,7 +19,7 @@ export const PaymentController = {
   createPayment: async (req: Request, res: Response) => {
     const { userId, amount } = req.body;
     const embed_data = {
-      redirecturl: "https://novasix-be.onrender.com/wallet",
+      redirecturl: "https://novasix.vercel.app/wallet",
     };
 
     const items = [
